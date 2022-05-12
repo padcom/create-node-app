681c7514bd93d730098b8d48ac39b23734050e0d
Matthias Hryniszak <padcom@gmail.com>
2022-05-13 01:55:49 +0200

    Added updating of package.json

--- node-aut.sh
+++ node-aut.sh
@@ -8,6 +8,16 @@ describe('Example test suite', () => {
 })
 EOM
 
+cat << EOM | node > package.json-1
+const packageJson = require('./package.json')
+packageJson.scripts['test'] = 'jest'
+packageJson.scripts['test:watch'] = 'jest --watchAll'
+console.log(JSON.stringify(packageJson, null, 2))
+EOM
+rm -f package.json
+mv package.json-1 package.json
+
+
 npm install --save-dev jest @types/jest ts-jest
 npx ts-jest config:init
-npx jest
+npm test
--- node-cna.sh
+++ node-cna.sh
@@ -1,12 +1,22 @@
 #!/bin/bash -e
 
 npm init -y
+
 cat << EOM > main.ts
 #!/usr/bin/env -S npx ts-node
 
 console.log('Hello!')
 EOM
 chmod +x main.ts
+
+cat << EOM | node > package.json-1
+const packageJson = require('./package.json')
+packageJson.main = 'main.ts'
+packageJson.scripts['start'] = 'ts-node main.ts'
+console.log(JSON.stringify(packageJson, null, 2))
+EOM
+rm -f package.json
+mv package.json-1 package.json
 
 npm install --save-dev typescript @types/node ts-node
 npx tsc --init
