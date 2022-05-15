e011c38600981cc1e2044fdbef4e9b50b5efa1a8
Matthias Hryniszak <padcom@gmail.com>
2022-05-15 03:40:20 +0200

    Added summary for linting

--- actions/SummaryCommand.mjs
+++ actions/SummaryCommand.mjs
@@ -17,6 +17,12 @@ export class SummaryCommand extends Action {
       console.log('or you can run them continuously:\n')
       console.log(chalk.green('$'), chalk.white('npm run test:watch\n'))
     }
+    if (options.initEslint) {
+      console.log('You can lint your project like that:\n')
+      console.log(chalk.green('$'), chalk.white('npm run lint\n'))
+      console.log('or you can try fixing all the linter problems automatically:\n')
+      console.log(chalk.green('$'), chalk.white('npm run lint:fix\n'))
+    }
 
     return { summaryDisplayed: true }
   }
